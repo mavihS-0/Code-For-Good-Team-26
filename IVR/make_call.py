cba7f5efda4c5c3266c71b94ace94348f91ea7d8
mavihS-0 <shivam9753singh@gmail.com>
2023-07-16 01:31:20 +0530

    rewriting code in python to run using flask (halfway through)

--- IVR/make_call.py
+++ IVR/make_call.py
@@ -13,7 +13,7 @@ call = client.calls.create(
     twiml='''<?xml version="1.0" encoding="UTF-8"?>
     <Response>
         <Say language="hi-IN">नमस्ते</Say>
-        <Gather input="speech dtmf" timeout="3" numDigits="1" action="https://hello-voice-8069-pkcoy6.twil.io/response-handle" method="GET">
+        <Gather input="speech dtmf" timeout="3" numDigits="1" action="https://2e8c-2405-201-e010-fc37-7d-6913-cf4c-e610.ngrok-free.app/voice" method="GET">
             <Say language="hi-IN">अगर आपके घर आज पानी आया है तो एक दबाएँ। अगर आपके घर आज पानी नहीं आया है तो दो दबाएँ।</Say>
         </Gather>
     </Response>''',
