296cbc9d5a35b1fc66a20a2a5fd49d011abd0bc9
mavihS-0 <shivam9753singh@gmail.com>
2023-07-16 03:31:53 +0530

    removing cache

--- IVR/auth_tokens.py
+++
@@ -1,4 +0,0 @@
-# Authorize tokens for Twilio API and MongoDB (to be removed)
-account_sid = "ACf3c81a0eae438f340bfab47d24684f07"
-auth_token = "417806c1a80681062ce6c7468ffab81b"
-uri = "mongodb+srv://shivam:shivam@cluster0.mdmqurh.mongodb.net/?retryWrites=true&w=majority"
